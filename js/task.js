820ab943db918efa659dc2a335929648b8f0d537
Natalija_L <natalija.liske@gmail.com>
2023-06-04 22:09:57 +0300

    kintamieji3

--- js/task.js
+++ js/task.js
@@ -1,4 +1,7 @@
 console.clear();
+//Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
 
 const one = '1';
 const two = '2';
@@ -7,3 +10,34 @@ const three = '3';
 const all = (one + two + three);
 
 console.log(all);
+
+console.clear();
+//Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+
+const dogName = 'Kyra';
+const catName = 'Midnight';
+const fishName = 'Nemo';
+
+const all1 = (dogName + catName + fishName);
+console.log(all1);
+
+console.clear();
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const pupa = '12345';
+const braske = '67891';
+const slyva = '65895';
+
+const ogos = `${pupa[0]}.${braske[1]}.${slyva[2]}`;
+console.log(ogos);
+
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+
+
+
---
+++ js/task1.js
@@ -0,0 +1,13 @@
+//Susumuoti visus skaičiaus tipo kintamuosius
+//Rezultatą išvesti į console
+
+//Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+//Rezultatą išvesti į console
+
+//Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+//1-2+3-4+5
+//Rezultatą išvesti į console
+
+//Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
+
+
